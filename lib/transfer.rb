22319cdbace79823f73c70105bf8b287b03a8ab2
Rehman Ali <rehmanali001@gmail.com>
2021-06-02 17:46:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,16 +1,16 @@
 class Transfer
-  attr_accessor :status, :sender, :receiver, :amount 
+  attr_accessor :status, :sender, :receiver, :amount
   def initialize(sender, receiver, amount)
+    @status = 'pending'
     @sender = sender
     @receiver = receiver
     @amount = amount
-    @status = 'pending'
   end
-  
+
   def valid?
     @sender.valid? && @receiver.valid?
   end
-  
+
   def execute_transaction
       if (@sender.balance > @amount && @status == "pending" && valid?)
       @sender.balance -= @amount
@@ -22,12 +22,16 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
-  
+
   def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
-    end 
-  end 
+    end
+  end
+  attr_accessor :status
+  def initialize
+    @status = 'pending'
+  end
 end
