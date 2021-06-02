69f7dec03f85e776d600810d6595d7362ffee861
Rehman Ali <rehmanali001@gmail.com>
2021-06-02 20:07:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,18 +25,19 @@ class Transfer
       end
     end
 
-  def reject_transfer
-    self.status = "rejected"
-  end
+    def reverse_transfer
+        if valid? && receiver.balance > amount && self.status == "complete"
+          receiver.balance -= amount
+          sender.balance += amount
+          self.status = "reversed"
+        else
+          reject_transfer
+        end
+      end
 
-  def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "complete"
-      receiver.balance -= amount
-      sender.balance += amount
-      self.status = "reversed"
-    else
-      reject_transfer
-    end
-  end
+  def reject_transfer
+     self.status = "rejected"
+     "Transaction rejected. Please check your account balance."
+   end
 
 end
